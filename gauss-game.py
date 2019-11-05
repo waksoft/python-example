7bec64e53cfb70e5a9565853cb2cc666cd78853d
waksoft <waksoft@gmail.com>
2019-11-05 20:37:01 +0500

    Пример для статьи о PyCharm
    Пример для статьи о PyCharm

---
+++ gauss-game.py
@@ -0,0 +1,19 @@
+from random import randint
+
+def play():
+    random_int = randint(0, 100)
+
+    while True:
+        user_guess = int(input("Запишите целое число в диапазоне от 0 до 100? "))
+        if user_guess == random_int:
+            print(f"Вы угадали число ({random_int}). Поздравляю!")
+            break
+        if user_guess < random_int:
+            print("Ваше число меньше секретного.")
+            continue
+        if user_guess > random_int:
+            print("Выше число больше секретного.")
+            continue
+
+if __name__ == '__main__':
+    play()
